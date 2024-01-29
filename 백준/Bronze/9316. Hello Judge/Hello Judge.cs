41f92cc325849064b839cbf7634eace5ca337cf1
이주환 <43924035+leejh0469@users.noreply.github.com>
2024-01-30 02:16:54 +0900

    [Bronze IV] Title: Hello Judge, Time: 68 ms, Memory: 5512 KB -BaekjoonHub

---
+++ 백준/Bronze/9316. Hello Judge/Hello Judge.cs
@@ -0,0 +1,11 @@
+internal class Program
+    {
+        static void Main(string[] args)
+        {
+                int n = int.Parse(Console.ReadLine());
+		
+		for(int i = 0; i < n; i++){
+			Console.WriteLine("Hello World, Judge " + (i+1) + "!");
+		    }
+            }
+        }
