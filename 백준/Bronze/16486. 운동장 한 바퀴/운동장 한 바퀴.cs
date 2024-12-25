e643732740a9bb72d36e4cb0d3c91c29c4f3be27
이주환 <43924035+leejh0469@users.noreply.github.com>
2024-12-25 22:46:51 +0900

    [Bronze IV] Title: 운동장 한 바퀴, Time: 48 ms, Memory: 5500 KB -BaekjoonHub

---
+++ 백준/Bronze/16486. 운동장 한 바퀴/운동장 한 바퀴.cs
@@ -0,0 +1,15 @@
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int d1 = int.Parse(Console.ReadLine());
+            int d2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(d1 * 2 + d2 * 2 * 3.141592);
+        }
+    }
+}
+
+
