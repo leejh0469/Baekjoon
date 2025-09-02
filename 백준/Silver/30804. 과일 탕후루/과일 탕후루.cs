b1f3855075675d13185004717e4f3db412d3cd72
이주환 <43924035+leejh0469@users.noreply.github.com>
2025-09-02 15:27:15 +0900

    [Silver II] Title: 과일 탕후루, Time: 96 ms, Memory: 16704 KB -BaekjoonHub

--- 백준/Silver/30804. 과일 탕후루/과일 탕후루.cs
+++ 백준/Silver/30804. 과일 탕후루/과일 탕후루.cs
@@ -1,57 +1,58 @@
-using System.Text;
+using System;
+using System.Collections.Generic;
 
 namespace ConsoleApp2
 {
     internal class Program
     {
-        static int N;
-        static int lk, rk;
-        static int max = 0;
-        static int[] input;
-
         static void Main(string[] args)
         {
-            N = int.Parse(Console.ReadLine());
+            int N = int.Parse(Console.ReadLine());
+            int[] input = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 
-            input = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-
-            lk = 0;
-            rk = 0;
-
-            Dictionary<int, int> dic = new Dictionary<int, int>();
-
-            while (rk < N && lk <= rk)
+            // N이 2 이하일 경우, 전체 배열이 정답
+            if (N <= 2)
             {
-                if(dic.Count <= 2)
+                Console.WriteLine(N);
+                return;
+            }
+
+            int left = 0;
+            int maxLength = 0;
+            // 딕셔너리를 사용하여 현재 윈도우에 있는 숫자의 개수를 추적
+            var counts = new Dictionary<int, int>();
+
+            // right 포인터가 배열 끝까지 이동
+            for (int right = 0; right < N; right++)
+            {
+                // 1. 윈도우 확장: right 포인터의 숫자를 윈도우에 추가
+                int num = input[right];
+                if (counts.ContainsKey(num))
                 {
-                    int key = input[rk];
-                    if( dic.ContainsKey(key) )
-                    {
-                        dic[key]++;
-                    }
-                    else
-                    {
-                        dic.Add(key, 1);
-                    }
-
-                    if (dic.Count <= 2)
-                        max = Math.Max(max, rk - lk + 1);
-
-                    rk++;
+                    counts[num]++;
                 }
                 else
                 {
-                    int key = input[lk];
-
-                    dic[key]--;
-                    if (dic[key] == 0)
-                        dic.Remove(key);
-
-                    lk++;
+                    counts.Add(num, 1);
                 }
+
+                // 2. 윈도우 축소: 윈도우 내 숫자의 종류가 2개를 초과하면, 2개가 될 때까지 left 포인터를 이동
+                while (counts.Count > 2)
+                {
+                    int leftNum = input[left];
+                    counts[leftNum]--;
+                    if (counts[leftNum] == 0)
+                    {
+                        counts.Remove(leftNum);
+                    }
+                    left++;
+                }
+
+                // 3. 결과 갱신: 현재 유효한 윈도우의 길이로 최대 길이를 갱신
+                maxLength = Math.Max(maxLength, right - left + 1);
             }
 
-            Console.WriteLine(max);
+            Console.WriteLine(maxLength);
         }
     }
 }
