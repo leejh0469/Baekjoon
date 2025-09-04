2a98256c825e24fa99d02318cc8300a96630ccd6
이주환 <43924035+leejh0469@users.noreply.github.com>
2025-09-04 15:24:18 +0900

    [Gold IV] Title: DSLR, Time: 3552 ms, Memory: 36836 KB -BaekjoonHub

--- 백준/Gold/9019. DSLR/DSLR.cs
+++ 백준/Gold/9019. DSLR/DSLR.cs
@@ -1,86 +1,76 @@
-namespace ConsoleApp1
+using System.Text;
+
+// 백준 9019번: DSLR
+internal class Program
 {
-    internal class Program
-    { 
-        static void Main(string[] args)
+    static void Main(string[] args)
+    {
+        // 빠른 출력을 위해 StringBuilder 사용
+        var sb = new StringBuilder();
+        int T = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < T; i++)
         {
-            int T = int.Parse(Console.ReadLine());
+            int[] input = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            int A = input[0];
+            int B = input[1];
 
-            for (int i = 0; i < T; i++)
+            string[] commands = new string[10000]; // 각 숫자에 도달하기 위한 명령어 경로
+            bool[] visited = new bool[10000];
+            Queue<int> q = new Queue<int>();
+
+            q.Enqueue(A);
+            visited[A] = true;
+            commands[A] = ""; // 시작점의 경로는 비어있음
+
+            while (q.Count > 0)
             {
-                int[] input = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+                int current = q.Dequeue();
 
-                int A = input[0];
-                int B = input[1];
-
-                string[] s = new string[10000];
-                bool[] visited = new bool[10000];
-                int resister = 0;
-
-                Queue<int> q = new Queue<int>();
-                q.Enqueue(A);
-                visited[A] = true;
-
-                while (q.Count > 0)
+                // 큐에서 꺼낸 값이 목표와 같다면, 탐색 종료. 이것이 최단 경로임.
+                if (current == B)
                 {
-                    int x = q.Dequeue();
-
-                    if (x == B)
-                        break;
-
-                    int cal;
-
-                    cal = (x * 2) % 10000;
-                    if (!visited[cal])
-                    {
-                        q.Enqueue(cal);
-                        visited[cal] = true;
-                        s[cal] = s[x] + 'D';
-                    }
-
-                    if (x == 0)
-                        cal = 9999;
-                    else
-                        cal = x - 1;
-
-                    if (!visited[cal])
-                    {
-                        q.Enqueue(cal);
-                        visited[cal] = true;
-                        s[cal] = s[x] + 'S';
-                    }
-
-                    int[] d = new int[4];
-
-                    int temp = x;
-                    int div = 10;
-
-                    for(int j = 0; j < 4; j++)
-                    {
-                        d[j] = temp % div;
-                        temp /= div;
-                    }
-
-                    cal = ((d[2] * 10 + d[1]) * 10 + d[0]) * 10 + d[3];
-
-                    if (!visited[cal])
-                    {
-                        q.Enqueue(cal);
-                        visited[cal] = true;
-                        s[cal] = s[x] + 'L';
-                    }
-
-                    cal = ((d[0] * 10 + d[3]) * 10 + d[2]) * 10 + d[1];
-                    if (!visited[cal])
-                    {
-                        q.Enqueue(cal);
-                        visited[cal] = true;
-                        s[cal] = s[x] + 'R';
-                    }
+                    sb.AppendLine(commands[B]);
+                    break;
                 }
 
-                Console.WriteLine(s[B]);
+                // 1. D 명령어 (Double)
+                int next = (current * 2) % 10000;
+                if (!visited[next])
+                {
+                    q.Enqueue(next);
+                    visited[next] = true;
+                    commands[next] = commands[current] + "D";
+                }
+
+                // 2. S 명령어 (Subtract)
+                next = (current == 0) ? 9999 : current - 1;
+                if (!visited[next])
+                {
+                    q.Enqueue(next);
+                    visited[next] = true;
+                    commands[next] = commands[current] + "S";
+                }
+
+                // 3. L 명령어 (Left Rotate)
+                next = (current % 1000) * 10 + (current / 1000);
+                if (!visited[next])
+                {
+                    q.Enqueue(next);
+                    visited[next] = true;
+                    commands[next] = commands[current] + "L";
+                }
+
+                // 4. R 명령어 (Right Rotate)
+                next = (current % 10) * 1000 + (current / 10);
+                if (!visited[next])
+                {
+                    q.Enqueue(next);
+                    visited[next] = true;
+                    commands[next] = commands[current] + "R";
+                }
             }
         }
+        Console.Write(sb.ToString());
     }
 }
